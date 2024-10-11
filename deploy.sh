8100929b5d8363bea4a7f55358fea20f9005af3f
Nathaniel Paulus <nathaniel_paulus@sil.org>
2024-10-11 12:40:57 -0400

    Add workflow to deploy site preview

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,8 @@ set -euxo pipefail
 # NETLIFY_AUTH_TOKEN must be set in the environment
 export NETLIFY_SITE_ID=aba3890d-2b7b-4144-883f-7c600965db82
 
-npx netlify deploy --dir build
+if [ "$#" -eq 1 ] && [ "$1" == "--github-action-preview-subdomain" ]; then
+    npx netlify deploy --dir build --alias github-action-preview
+else
+    npx netlify deploy --dir build
+fi
