b96386d4d88fc287ad68b05d6f758f377a7994d3
kylebuss <kyle_buss@sil.org>
2025-04-14 07:13:38 -0500

    Add anchor and image validation checks

---
+++ locales.ts
@@ -0,0 +1,27 @@
+export interface HelpLocales {
+  docusaurus: string;
+  crowdin: string;
+}
+
+export const helpLocales = [
+  {
+    docusaurus: "de",
+    crowdin: "de",
+  },
+  {
+    docusaurus: "es",
+    crowdin: "es-ES",
+  },
+  {
+    docusaurus: "fr",
+    crowdin: "fr",
+  },
+  {
+    docusaurus: "pt-BR",
+    crowdin: "pt-BR",
+  },
+  {
+    docusaurus: "id",
+    crowdin: "id",
+  },
+] as HelpLocales[];
